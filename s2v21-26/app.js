6bd485f23e47218aa07d5b8b2fd49dd1d6aa518b
Maximiliano Luci <maximiliano.luci@icloud.com>
2023-04-14 12:03:09 +0200

    until 26

--- s2v21-26/app.js
+++ s2v21-26/app.js
@@ -2,8 +2,27 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
+      confirmedName: "",
     };
+  },
+  methods: {
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm(event) {
+      alert("Submitted!");
+    },
+    setName(event) {
+      this.name = event.target.value;
+    },
+    addCounter(num) {
+      this.counter = this.counter + num;
+    },
+    substractCounter(num) {
+      this.counter = this.counter - num;
+    },
   },
 });
 
-app.mount('#events');
+app.mount("#events");
