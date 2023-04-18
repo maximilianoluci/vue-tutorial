c05291878ff764021877fe23f2136da891fbd872
Maximiliano Luci <maximiliano.luci@icloud.com>
2023-04-18 11:08:59 +0200

    æ

--- s2v27-32/app.js
+++ s2v27-32/app.js
@@ -2,12 +2,21 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: "",
     };
   },
+  computed: {
+    fullName() {
+      if (this.name === "") {
+        return "";
+      }
+
+      return this.name + " " + "Schwarzmüller";
+    },
+  },
   methods: {
-    setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
+    setName(event) {
+      this.name = event.target.value;
     },
     add(num) {
       this.counter = this.counter + num;
@@ -15,8 +24,11 @@ const app = Vue.createApp({
     reduce(num) {
       this.counter = this.counter - num;
       // this.counter--;
-    }
-  }
+    },
+    resetInput() {
+      this.name = "";
+    },
+  },
 });
 
-app.mount('#events');
+app.mount("#events");
